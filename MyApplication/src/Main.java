e5cfeea5035da730f82aa318dd03f98fe052f5b1
karthik <karthik@star-systems.in>
2019-04-30 12:51:48 +0530

    Initial commit

---
+++ MyApplication/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println ("This is the Main class");
+        System.out.println ("This class is converted into a JAR Main.jar");
+    }
+}
---
+++ MyApplication/src/com/demo/MyClass.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println ("This is the dependent class and depends on the Main class");
+        System.out.println ("This class is converted into JAR file MyApplication.jar");
+    }
+}
